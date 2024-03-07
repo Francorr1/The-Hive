c366d5fea523f425cb1a45bcd2d4677ce5327818
Franco Correa <6203@hbtn.uy>
2024-03-07 20:22:30 -0300

    Hope this works

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(-vInput, 0f, hInput) * speed * Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
